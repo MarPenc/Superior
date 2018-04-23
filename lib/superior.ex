ac02b184537d19a140cccfe72d5805fb96d3fb52
Martin Penckert <mp@penckert.com>
2018-04-23 13:36:18 +0200

    Added AoS functionality

--- lib/superior.ex
+++ lib/superior.ex
@@ -5,15 +5,10 @@ defmodule Superior do
 
   @doc """
     Provides a new deck of 52 cards.
-
-    ## Examples
-
-      iex> hd(Superior.new_deck)
-      "Ace of Spades"
-
   """
   def new_deck do
-    values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+    values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+              "Jack", "Queen", "King"]
     suits  = ["Spades", "Clubs", "Diamonds", "Hearts"]
 
     for suit <- suits, value <- values do
@@ -21,5 +16,39 @@ defmodule Superior do
     end
   end
 
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  @doc """
+    Deals the first card of any given deck.
+    If the dealt card is the Ace of Spades the Player leaves the game,
+    the card is put back into the deck and the deck is shuffled.
+
+    ## Examples
+
+    iex> deck1 = ["Queen of Diamonds", "6 of Hearts", "5 of Hearts", "7 of Spades"]
+    iex> Superior.deal(deck1)
+    iex> {hand, deck11} = Superior.deal(deck1)
+    iex> hand
+    ["Queen of Diamonds"]
+    iex> deck11
+    ["6 of Hearts", "5 of Hearts", "7 of Spades"]
+    iex> deck2 = ["Ace of Spades", "2 of Spades", "3 of Spades", "4 of Spades"] 
+    iex> Superior.deal deck2
+    iex> {hand, deck21} = Superior.deal(deck2)
+    iex> hand
+    ["Game Over"]
+    iex> Enum.member?(deck21, "Ace of Spades")
+    true
+
+  """
+  def deal(deck) do
+    case Enum.split(deck, 1) do
+      { ["Ace of Spades"], _ } -> { ["Game Over"], Superior.shuffle(deck) }
+      { hand, deck } -> { hand, deck }
+    end
+
+  end
 
 end
--- test/superior_test.exs
+++ test/superior_test.exs
@@ -2,6 +2,10 @@ defmodule SuperiorTest do
   use ExUnit.Case
   doctest Superior
 
+  def test_deck do
+    ["Two of Clubs", "Three of Diamonds"]
+  end
+
   test "provides a deck of 52 cards" do
     assert length(Superior.new_deck) == 52
   end
@@ -16,8 +20,19 @@ defmodule SuperiorTest do
     refute deck == shuffled_deck
   end
 
-  test "if requested, Superior deals a new card" do
-    assert length(Superior.new_card) == 1
+  test "deals a hand of exactly one card" do
+    {hand, _remaining_deck} = SuperiorTest.test_deck |> Superior.deal
+    assert length(hand) == 1
+  end
+
+  test "ends game if Ace of Spades is drawn" do
+    { hand, _deck } = ["Ace of Spades"] |> Superior.deal 
+    assert hand == ["Game Over"]
+  end
+
+  test "returns the Ace of Spades after its been dealt" do
+    { _, remaining_deck } = Superior.new_deck |> Superior.deal
+    assert Enum.member?(remaining_deck, "Ace of Spades") == :true
   end
 
 end
